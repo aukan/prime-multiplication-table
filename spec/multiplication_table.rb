17af8c75603386c5ad17b5fdaf32e96209188c77
Pablo Gonzalez <pbglezc@gmail.com>
2015-05-10 23:03:32 -0500

    Add MultiplicationTable spec

--- spec/multiplication_table.rb
+++ spec/multiplication_table.rb
@@ -1,12 +1,27 @@
+require "#{File.dirname(__FILE__)}/../lib/multiplication_table"
 
 RSpec.describe MultiplicationTable do
   describe '#table' do
-    it 'returns the multiplication table' do
+    it 'should return the multiplication table' do
       mt = MultiplicationTable.new [2, 4], [5, 6]
-      expect(mt.table[0, 0]).to eq(10)
-      expect(mt.table[0, 1]).to eq(20)
-      expect(mt.table[0, 1]).to eq(12)
-      expect(mt.table[0, 1]).to eq(24)
+      expect(mt.generate_table[0][0]).to eq(10)
+      expect(mt.generate_table[0][1]).to eq(20)
+      expect(mt.generate_table[1][0]).to eq(12)
+      expect(mt.generate_table[1][1]).to eq(24)
+    end
+
+    it 'should returns an empty multiplication table when arguments are empty' do
+      mt = MultiplicationTable.new [], []
+      expect(mt.generate_table[0][0]).to eq(nil)
+    end
+
+    it 'should duplicate row if column is not given' do
+      mt = MultiplicationTable.new [4, 5]
+
+      expect(mt.generate_table[0][0]).to eq(16)
+      expect(mt.generate_table[0][1]).to eq(20)
+      expect(mt.generate_table[1][0]).to eq(16)
+      expect(mt.generate_table[1][1]).to eq(20)
     end
   end
 end
